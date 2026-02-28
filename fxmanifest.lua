f62c8e01e3dc750bc2559bc8f775786e60360c5d
Giovanni <giovaniicc97@gmail.com>
2026-02-28 22:42:16 +0100

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Giovanni'
+description 'Giovanni - Speisekarten'
+version '1.0.0'
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html',
+    'html/style.css',
+    'html/script.js',
+    'html/img/*.jpg'
+}
+
+client_scripts {
+    'client.lua'
+}
